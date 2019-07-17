942a2e05430bedd9002a47096015c17c086b5d52
Jaye <joshua.hackett93@gmail.com>
2019-07-17 21:40:17 +0100

    added styled components and theme

---
+++ pages/theme.js
@@ -0,0 +1,47 @@
+export default {
+
+    // typography
+    darkText: "#1E2428",
+    lightText: "#555F66",
+
+    // links
+    link: "#20825F",
+
+    // backgrounds
+    shadow: "#D8DDE0",
+    background: "#F0F3F2",
+    white: "#ffffff",
+
+    // psuedostates
+    focus: "#ffb81C",
+    activeCard: "#fafafa",
+
+    // service colours
+    active: "#98D9CA",
+    support: "#F7ADB6",
+    learning: "#BBEAFA",
+    cultural: "#FAC9A3",
+    social: "#A9BAE1",
+
+    maxWidth: 1000,
+
+    // breakpoints
+    smartphone: 250,
+    tablet: 750,
+    desktop: 1000,
+    largeDesktop: 1500,
+
+    // base font sizes
+    title: "2em",
+    subtitle: "1.5em",
+    lede: "1.3em",
+    paragraph: "1em",
+    small: "0.95em",
+
+    // desktop font sizes
+    titleLarge: "2.813em",
+    subtitleLarge: "1.875em",
+    ledeLarge: "1.5em",
+    paragraphLarge: "1.188em",
+    smallLarge: "1.125em"
+}
